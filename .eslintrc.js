c3eb1b00012059646b9de8a26c76dc1ffc61cbb0
shengbo wang <shuichen17@gmail.com>
2019-05-17 15:27:17 -0700

    getting-description

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,33 @@
-/**
- * These rules enforce the Hack Reactor Style Guide
- *
- * Visit this repo for more information:
- *   https://github.com/reactorcore/eslint-config-hackreactor
- */
-
 module.exports = {
-  extends: './node_modules/eslint-config-hackreactor/index.js'
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    mongo: true,
+  },
+  extends: 'airbnb',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'import/extensions': {
+      jsx: 'always',
+    },
+    'no-console': 0,
+    'prefer-const': 0,
+    'consistent-return': 0,
+    'no-unused-vars': 0,
+    'no-shadow': 0,
+  },
 };
---
+++ client/src/index.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/newline-after-import */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+ReactDOM.render(<App />, document.getElementById('app'));
